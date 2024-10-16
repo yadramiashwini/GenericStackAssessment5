bf8efa30495d45fa960fadf0d44d38568f8e317a
Ashwini Yadrami <yadramiash192380@gmail.com>
2024-10-16 21:39:18 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace GenericStackAssessment5
+{
+    public class GenericStack
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> elements = new Stack<string>();
+            elements.Push("Ash");
+            elements.Push("pooja");
+            elements.Push("sonyyyy");
+
+            foreach (var element in elements)
+            {
+                Console.WriteLine(element);
+            }
+            elements.Peek();
+            Console.WriteLine("display array elements:");
+            elements.Pop();
+
+            Console.WriteLine("after popping elements:");
+
+            foreach(var element in elements)
+            {
+                Console.WriteLine(element);
+            }
+
+
+        }
+    }
+}
